971dd8953bab1c86572d5c79bb4c381b2a371fd7
juninhoww2 <alqadianoKira@gmail.com>
2020-11-26 03:08:18 -0300

    Initial Commit

---
+++ tick/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './global.css';
+import ReactDOM from 'react-dom';
+
+function App() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));}
+
+setInterval(App, 1000);
+
+export default App;
